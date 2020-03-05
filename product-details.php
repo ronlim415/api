a0811a529bacb80271032baa826db9a92574f374
ronlim415 <60123575+ronlim415@users.noreply.github.com>
2020-03-05 11:07:37 +0800

    Update product-details.php

--- product-details.php
+++ product-details.php
@@ -36,7 +36,7 @@ $value = $list;
         <td><?php echo $value['description'];?></td>
         <td><?php echo $value['price'];?></td>
         <td><?php echo $value['category_id'];?></td>
-        <td><a href="form_update.php?id=<?php echo $id ?>">Update</a></td>
+        <td><a href="index.php?navigation=update&id=<?php echo $id ?>">Update</a></td>
         <td><a href="pro_delete.php?id=<?php echo $id ?>">Delete</a></td>
     </tr>
 
