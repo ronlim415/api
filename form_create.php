11ce647c96e4ad05a0f7b3815539f188a8e85ad7
ronlim415 <60123575+ronlim415@users.noreply.github.com>
2020-03-09 22:12:01 +0800

    Update form_create.php

--- form_create.php
+++ form_create.php
@@ -5,8 +5,9 @@
 	$category = $data['records'];
 ?>
 
-<form action="pro_create.php" method="POST">
 <h1> Create Product </h1>
+
+<form action="pro_create.php" method="POST">
 <input type="text" name="name" placeholder="name"/>
 <input type="text" name="description" placeholder="description"/>
 <input type="text" name="price" placeholder="price"/>
