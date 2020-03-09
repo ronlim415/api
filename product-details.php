c8f0c8b4cd6670a4b256b9a82eefcd401ee32d5b
ronlim415 <60123575+ronlim415@users.noreply.github.com>
2020-03-09 21:42:48 +0800

    Update product-details.php

--- product-details.php
+++ product-details.php
@@ -12,32 +12,24 @@ $value = $list;
     <head>  
       <link rel="stylesheet" type="text/css" href="style.css">
     </head>
-
-
-    <div class="navbar">
-        <a href="index.php?navigation=product">Show</a>
-        <a href="index.php?navigation=categories">Category</a>
-        <a href="index.php?navigation=create">Create</a>
-        <a href="index.php?navigation=delete">Delete</a>
-    </div>
-
+    
 <h1> Product Details </h1>
 
 <table>
     <tr>
-        <td>Product</td>
-        <td>Description</td>
-        <td>Price</br>
-        <td>Category ID</td>
+        <br>Product</br>
+        <br>Description</br>
+        <br>Price</br>
+        <br>Category ID</br>
     </tr>
 
     <tr>
-        <td><?php echo $value['name'];?></td>
-        <td><?php echo $value['description'];?></td>
-        <td><?php echo $value['price'];?></td>
-        <td><?php echo $value['category_id'];?></td>
-        <td><a href="index.php?navigation=update&id=<?php echo $id ?>">Update</a></td>
-        <td><a href="pro_delete.php?id=<?php echo $id ?>">Delete</a></td>
+        <br><?php echo $value['name'];?></br>
+        <br><?php echo $value['description'];?></br>
+        <br><?php echo $value['price'];?></br>
+        <br><?php echo $value['id'];?></br>
+        <br><a href="index.php?navigation=update&id=<?php echo $id ?>">Update</a></br>
+        <br><a href="pro_delete.php?id=<?php echo $id ?>">Delete</a></br>
     </tr>
 
 </table>
